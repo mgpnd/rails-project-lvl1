68be45352d20f55485ffed98905bbc24d965917a
Artem Rashev <mgpnd.personal@gmail.com>
2021-09-02 18:37:12 +0300

    Add form inputs generator

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,13 +7,14 @@ require_relative 'hexlet_code/form'
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(_, url: nil)
-    form = Form.new
+  def self.form_for(object, url: nil)
+    form = Form.new(object)
     yield form
 
     action = url.nil? ? '#' : url
 
     Tag.build('form', action: action, method: 'post', options: { multiline: true }) do
+      form.to_s
     end
   end
 end
--- lib/hexlet_code/form.rb
+++ lib/hexlet_code/form.rb
@@ -2,5 +2,53 @@
 
 module HexletCode
   class Form
+    attr_reader :object, :output
+
+    def initialize(object)
+      @object = object
+      @output = []
+    end
+
+    def input(name, as: nil, collection: nil)
+      output << input_for(name, as: as, collection: collection)
+    end
+
+    def select(name, options, value: nil)
+      Tag.build('select', name: name, options: { multiline: true }) do
+        options.map do |opt_value|
+          attributes = { value: opt_value }
+          attributes[:selected] = true if opt_value == value
+          Tag.build('option', attributes) { opt_value }
+        end.join("\n")
+      end
+    end
+
+    def textarea(name, value, cols: 20, rows: 40)
+      Tag.build(
+        'textarea',
+        name: name,
+        cols: cols,
+        rows: rows
+      ) { value }
+    end
+
+    def to_s
+      output.join("\n")
+    end
+
+    private
+
+    def input_for(name, as: nil, collection: nil)
+      value = object.public_send(name)
+
+      case as
+      when :select
+        select(name, collection, value: value)
+      when :text
+        textarea(name, value)
+      else
+        Tag.build('input', type: 'text', name: 'name', value: value)
+      end
+    end
   end
 end
